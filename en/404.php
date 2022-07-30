1fcf227ee19a450081b713997d89532eff38d10f
JazDob <dobantonj@hotmail.com>
2022-07-30 12:50:51 -0300

    404

---
+++ en/404.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Page description -->
+<meta name="description" content="Error page not found -website Fiorella Roetto: actress and independent producer. Student of Bachelor of Acting at National University of Arts">
+<!-- Bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<!-- Bootstrap Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+<!-- CSS only -->
+<link rel="stylesheet" type="text/css" href="./../styles/styles.css" />
+<!--Fuente inicio-->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap" rel="stylesheet">
+<!--Fuente fin-->
+    <!--Favicon inicio-->
+    <link rel="apple-touch-icon" sizes="57x57" href="./../fricon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="./../fricon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="./../fricon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="./../fricon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="./../fricon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="./../fricon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="./../fricon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="./../fricon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="./../fricon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="./fricon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./../fricon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="./../fricon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./../fricon/favicon-16x16.png">
+    <link rel="manifest" href="./../fricon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="./../fricon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <!--Favicon fin-->
+    <title>Error 404 ∙ Fiorella Roetto | Artista</title>
+</head>
+<body>
+    <div class="container bodyBox">
+        <h1>404</h1>
+        <h2>Page not found</h2>
+        <h2>Sorry, the page you are searching for does not exist.</h2>
+        <a class="miBoton" aria-current="page" href="./index.html">
+    </div>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
